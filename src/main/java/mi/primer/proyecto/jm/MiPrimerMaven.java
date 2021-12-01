14c642be74f769a83bafc84c278c13b399fb60c9
jkmoncayo <jkmoncayo@Joss>
2021-11-30 20:43:56 -0500

    Taller 2 mi_proyecto_jm

---
+++ src/main/java/mi/primer/proyecto/jm/MiPrimerMaven.java
@@ -0,0 +1,13 @@
+package mi.primer.proyecto.jm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MiPrimerMaven {
+
+	public static void main(String[] args) {
+System.out.println("Hola Mundo");
+List<Object> cv = new ArrayList<>();
+	}
+
+}
