fd12b44303d22102d6850f7271e3fc6090692ffd
jkmoncayo <jkmoncayo@Joss>
2021-12-13 17:02:25 -0500

    TAREA 2: IoC, DI con modelo de interfaz y clase anónima

--- src/main/java/com/ec/edu/inyeccion/dependencias/tipos/MainFramework.java
+++ src/main/java/com/ec/edu/inyeccion/dependencias/tipos/MainFramework.java
@@ -2,15 +2,12 @@ package com.ec.edu.inyeccion.dependencias.tipos;
 
 import java.util.Scanner;
 
-
-
-
 public class MainFramework {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scannerInt = new Scanner(System.in);
-		
+
 		System.out.println("Ingrese tipo de cuenta");
 		int tipo = scannerInt.nextInt();
 
@@ -20,8 +17,22 @@ public class MainFramework {
 			cuenta = new CuentaCorriente();
 		} else if (tipo == 2) {
 			cuenta = new CuentaAhorros();
-		} else if (tipo == 3){
+		} else if (tipo == 3) {
 			cuenta = new CuentaFuturo();
+		} else {
+			cuenta = new ICuenta() {
+
+				@Override
+				public void calcularDescuento(String numero) {
+					System.out.println("Cuenta por defecto, sin descuento habilitado");
+
+				}
+
+				@Override
+				public void calcularBeneficio() {
+					System.out.println("Cuenta por defecto, sin beneficios programados");
+				}
+			};
 		}
 
 		GestorCuentas gestor = new GestorCuentas(cuenta);
