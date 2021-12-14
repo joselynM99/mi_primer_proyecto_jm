a35a7efd0be153db6846ba07ac26ba1bade03edf
jkmoncayo <jkmoncayo@Joss>
2021-12-13 19:13:37 -0500

    Taller 6 (SpringFramework)

---
+++ src/main/java/com/ec/inyeccion/dependencias/framework/Cuenta.java
@@ -0,0 +1,23 @@
+package com.ec.inyeccion.dependencias.framework;
+
+public class Cuenta {
+	private String numero;
+	private int saldo;
+	
+	
+	//Metodos SET y GET
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public int getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+	
+
+}
---
+++ src/main/java/com/ec/inyeccion/dependencias/framework/Main.java
@@ -0,0 +1,5 @@
+package com.ec.inyeccion.dependencias.framework;
+
+public class Main {
+
+}
