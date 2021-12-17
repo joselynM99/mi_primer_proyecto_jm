5cd98d8fbc4dab0f4af00325592e80fec98fbf82
jkmoncayo <jkmoncayo@Joss>
2021-12-17 18:37:10 -0500

    prueba 3

--- src/main/java/com/prueba/TestMain.java
+++ src/main/java/com/prueba/TestMain.java
@@ -3,7 +3,8 @@ package com.prueba;
 public class TestMain {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("cambios ");
+		System.out.println("cambio 3 ");
+		System.out.println("cambio 4 ");
 	}
 
 }
