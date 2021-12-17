6404fdb74db3ad6b6525dcb4a63ab3decc0d31dd
jkmoncayo <jkmoncayo@Joss>
2021-12-17 18:49:29 -0500

    cambios de prueba

--- src/main/java/com/prueba/TestMain.java
+++ src/main/java/com/prueba/TestMain.java
@@ -4,6 +4,7 @@ public class TestMain {
 	public static void main(String[] args) {
         System.out.println("Prueba de edicion de kenlly");
         System.out.println("Segundo cambio");
+        System.out.println("cambio3");
 	}
 
 }
