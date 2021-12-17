ba0c207c67a5ff90549c853d11e9cc5dd7ee85ad
KenllyChacon <klchacon@uce.edu.ec>
2021-12-17 18:40:18 -0500

    corregido

--- src/main/java/com/prueba/TestMain.java
+++ src/main/java/com/prueba/TestMain.java
@@ -2,9 +2,7 @@ package com.prueba;
 
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
-		System.out.println("cambio 3 ");
-		System.out.println("cambio 4 ");
+        System.out.println("Prueba de edicion de kenlly");
 	}
 
 }
